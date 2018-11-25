460575f70baa870948cef64a88f623119d0367df
Beowulfv <beowolfv@gmail.com>
2018-11-24 21:11:22 -0500

    Add files via upload

---
+++ config.cpp
@@ -0,0 +1,29 @@
+class CfgXM8 
+{
+	extraApps[] = {"BRAmaRecipes"};
+	
+	class BRAmaRecipes 
+	{
+		controlID = 107000;
+		title = "Recipies";		
+		logo = "xm8Apps\BRAmaRecipes\BRAma.paa";
+		config = "xm8Apps\BRAmaRecipes\config.sqf";
+		onLoad = "xm8Apps\BRAmaRecipes\onLoad.sqf";
+		onOpen = "xm8Apps\BRAmaRecipes\onOpen.sqf";
+	};
+};
+
+class XM8_App05_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "xm8Apps\BRAmaRecipes\BRAma.paa";
+    text = "Recipies";
+    onButtonClick = "['BRAmaRecipes', 0] call ExileClient_gui_xm8_slide";
+    resource = "";
+};
+
+class CfgExileCustomCode 
+{
+	//xm8
+	ExileClient_gui_xm8_slide = "xm8Apps\CustomCode\ExileClient_gui_xm8_slide.sqf";
+	ExileClient_gui_xm8_show = "xm8Apps\CustomCode\ExileClient_gui_xm8_show.sqf";
+};
